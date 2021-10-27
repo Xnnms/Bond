9a2aeec92ed116300805d2d8f105f6936aa6e93a
Muhammad Ilham <ilhamgallagher633@gmail.com>
2021-10-27 22:59:18 +0700

    Initial commit

---
+++ src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,9 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray {
+    public static void main(String[] args) {
+        int[] arrInt = new int[]{1,2,3,4,5,6};
+
+        System.out.println(arrInt[2]);
+    }
+}
